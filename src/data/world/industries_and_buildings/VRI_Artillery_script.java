4a63d2c2373f91205b96b2335f9c77d34b6ed5a8
p2016p <cohenmatthew99@gmail.com>
2023-04-30 19:51:15 -0400

    added the industries

---
+++ src/data/world/industries_and_buildings/VRI_Artillery_script.java
@@ -0,0 +1,128 @@
+package data.world.industries_and_buildings;
+
+import com.fs.starfarer.api.campaign.econ.CommodityOnMarketAPI;
+import com.fs.starfarer.api.campaign.econ.Industry;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.campaign.econ.MarketImmigrationModifier;
+import com.fs.starfarer.api.impl.campaign.econ.impl.BaseIndustry;
+import com.fs.starfarer.api.impl.campaign.ids.Commodities;
+import com.fs.starfarer.api.impl.campaign.ids.Stats;
+import com.fs.starfarer.api.impl.campaign.population.PopulationComposition;
+import com.fs.starfarer.api.impl.campaign.rulecmd.salvage.MarketCMD;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VRI_Artillery_script extends BaseIndustry {
+
+    private static final float DEFENSE_BONUS = 4f;
+    private static final float POP_GROWTH = 10f;
+
+    public static List<String> SUPPRESSED_CONDITIONS = new ArrayList();
+
+    static {
+        SUPPRESSED_CONDITIONS.add("meteor_impacts");
+    }
+
+    @Override
+    public boolean isFunctional() {
+        return super.isFunctional();
+    }
+
+    @Override
+    public void apply() {
+        super.apply(true);
+
+        for(String s:SUPPRESSED_CONDITIONS){
+            if(market.hasCondition(s)){
+                market.suppressCondition(s);
+            }
+        }
+
+        int size = market.getSize();
+
+        this.demand(Commodities.CREW, size);
+        this.demand(Commodities.HEAVY_MACHINERY, size);
+        this.demand(Commodities.VOLATILES, size);
+
+        market.getStats().getDynamic().getMod(Stats.GROUND_DEFENSES_MOD).modifyMult("Aegis Artillery Network",1.5f);
+    }
+    @Override
+    public void unapply() {
+        super.unapply();
+
+        for(String s:SUPPRESSED_CONDITIONS){
+            if(market.hasCondition(s)){
+                market.unsuppressCondition(s);
+            }
+        }
+    }
+    protected boolean hasPostDemandSection(boolean hasDemand, IndustryTooltipMode mode) {
+        return mode != IndustryTooltipMode.NORMAL || isFunctional();
+    }
+    @Override
+    protected void addPostDemandSection(TooltipMakerAPI tooltip, boolean hasDemand, IndustryTooltipMode mode) {
+        if (mode != IndustryTooltipMode.NORMAL || isFunctional()) {
+            Color h = Misc.getHighlightColor();
+            float opad = 10f;
+
+            tooltip.addPara("Increases colony ground defenses by %s",opad, h, "1.5x");
+            tooltip.addPara("Counters the effects of %s.",opad, h, "Meteor Impacts");
+        }
+    }
+    @Override
+    protected int getBaseStabilityMod() {
+        return 3;
+    }
+
+    public String getNameForModifier() {
+
+        return "Aegis Artillery Network";
+    }
+    @Override
+    public String getCurrentImage() {
+        return super.getCurrentImage();
+    }
+    public boolean isDemandLegal(CommodityOnMarketAPI com) {
+        return true;
+    }
+    public boolean isSupplyLegal(CommodityOnMarketAPI com) {
+        return true;
+    }
+    @Override
+    protected void buildingFinished() {
+        super.buildingFinished();
+    }
+    @Override
+    protected void upgradeFinished(Industry previous) {
+        super.upgradeFinished(previous);
+    }
+    @Override
+    public void advance(float amount) {
+        super.advance(amount);
+
+    }
+
+    @Override
+    public boolean isAvailableToBuild() {
+        return false;
+    }
+    public boolean showWhenUnavailable() {
+        return false;
+    }
+    @Override
+    public boolean canImprove() {
+        return false;
+    }
+    @Override
+    public MarketCMD.RaidDangerLevel adjustCommodityDangerLevel(String commodityId, MarketCMD.RaidDangerLevel level) {
+        return level.next();
+    }
+    @Override
+    public MarketCMD.RaidDangerLevel adjustItemDangerLevel(String itemId, String data, MarketCMD.RaidDangerLevel level) {
+        return level.next();
+    }
+}
---
+++ src/data/world/industries_and_buildings/VRI_VolGen_Office_script.java
@@ -0,0 +1,152 @@
+package data.world.industries_and_buildings;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.*;
+import com.fs.starfarer.api.campaign.econ.*;
+import com.fs.starfarer.api.campaign.listeners.FleetEventListener;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import com.fs.starfarer.api.impl.campaign.econ.impl.BaseIndustry;
+import com.fs.starfarer.api.impl.campaign.econ.impl.MilitaryBase;
+import com.fs.starfarer.api.impl.campaign.events.OfficerManagerEvent;
+import com.fs.starfarer.api.impl.campaign.fleets.*;
+import com.fs.starfarer.api.impl.campaign.ids.*;
+import com.fs.starfarer.api.impl.campaign.population.PopulationComposition;
+import com.fs.starfarer.api.impl.campaign.rulecmd.salvage.MarketCMD;
+import com.fs.starfarer.api.impl.campaign.shared.SharedData;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.IntervalUtil;
+import com.fs.starfarer.api.util.Misc;
+import com.fs.starfarer.api.util.Pair;
+import data.world.systems.Avery;
+import data.world.systems.Royce;
+import data.world.systems.Uelyst;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class VRI_VolGen_Office_script extends BaseIndustry implements MarketImmigrationModifier {
+
+    private static final float DEFENSE_BONUS = 4f;
+    private static final float POP_GROWTH = 10f;
+
+    public static List<String> SUPPRESSED_CONDITIONS = new ArrayList();
+
+    static {
+        SUPPRESSED_CONDITIONS.add("hot");
+        SUPPRESSED_CONDITIONS.add("very_hot");
+        SUPPRESSED_CONDITIONS.add("cold");
+        SUPPRESSED_CONDITIONS.add("very_cold");
+        SUPPRESSED_CONDITIONS.add("irradiated");
+        SUPPRESSED_CONDITIONS.add("toxic_atmosphere");
+        SUPPRESSED_CONDITIONS.add("inimical_biosphere");
+        SUPPRESSED_CONDITIONS.add("pollution");
+        SUPPRESSED_CONDITIONS.add("dark");
+    }
+
+    @Override
+    public boolean isFunctional() {
+        return super.isFunctional();
+    }
+
+    @Override
+    public void apply() {
+        super.apply(true);
+
+        for(String s:SUPPRESSED_CONDITIONS){
+            if(market.hasCondition(s)){
+                market.suppressCondition(s);
+            }
+        }
+
+        int size = market.getSize();
+
+        this.demand(Commodities.ORGANICS, size+1);
+        this.demand(Commodities.HEAVY_MACHINERY, size+1);
+        this.demand(Commodities.METALS, size+1);
+
+        this.income.modifyMult(this.id,size,"Market Size");
+    }
+    @Override
+    public void unapply() {
+        super.unapply();
+
+        for(String s:SUPPRESSED_CONDITIONS){
+            if(market.hasCondition(s)){
+                market.unsuppressCondition(s);
+            }
+        }
+    }
+    protected boolean hasPostDemandSection(boolean hasDemand, IndustryTooltipMode mode) {
+        return mode != IndustryTooltipMode.NORMAL || isFunctional();
+    }
+    @Override
+    protected void addPostDemandSection(TooltipMakerAPI tooltip, boolean hasDemand, IndustryTooltipMode mode) {
+        if (mode != IndustryTooltipMode.NORMAL || isFunctional()) {
+            Color h = Misc.getHighlightColor();
+            float opad = 10f;
+
+            tooltip.addPara("Increases colony growth by %s",opad, h, String.valueOf(POP_GROWTH));
+            tooltip.addPara("Counters the effects of %s, %s, %s, %s, %s, %s, and %s.",opad, h, "Heat","Cold","Irradiated","Toxic Atmosphere", "Pollution", "Inimical Biosphere","Dark");
+        }
+    }
+    @Override
+    protected int getBaseStabilityMod() {
+        return 0;
+    }
+
+    public String getNameForModifier() {
+
+        return "VolGen Office";
+    }
+    @Override
+    public String getCurrentImage() {
+        return super.getCurrentImage();
+    }
+    public boolean isDemandLegal(CommodityOnMarketAPI com) {
+        return true;
+    }
+    public boolean isSupplyLegal(CommodityOnMarketAPI com) {
+        return true;
+    }
+    @Override
+    protected void buildingFinished() {
+        super.buildingFinished();
+    }
+    @Override
+    protected void upgradeFinished(Industry previous) {
+        super.upgradeFinished(previous);
+    }
+    @Override
+    public void advance(float amount) {
+        super.advance(amount);
+
+    }
+
+    @Override
+    public boolean isAvailableToBuild() {
+        return false;
+    }
+    public boolean showWhenUnavailable() {
+        return false;
+    }
+    @Override
+    public boolean canImprove() {
+        return false;
+    }
+    @Override
+    public MarketCMD.RaidDangerLevel adjustCommodityDangerLevel(String commodityId, MarketCMD.RaidDangerLevel level) {
+        return level.next();
+    }
+    @Override
+    public MarketCMD.RaidDangerLevel adjustItemDangerLevel(String itemId, String data, MarketCMD.RaidDangerLevel level) {
+        return level.next();
+    }
+
+    @Override
+    public void modifyIncoming(MarketAPI market, PopulationComposition incoming) {
+        incoming.getWeight().modifyFlat(this.getModId(), POP_GROWTH, "VolGen Branch");
+    }
+}
